b80d06a9be02ab6cea4c2e17e9d0354277e20a8d
Renato Cavalcanti <renato@cavalcanti.io>
2025-01-24 22:48:49 +0100

    add consumers

---
+++ src/main/java/demo/wallet/api/PublicWalletEvent.java
@@ -0,0 +1,18 @@
+package demo.wallet.api;
+
+import akka.javasdk.annotations.TypeName;
+
+public sealed interface PublicWalletEvent {
+  @TypeName("wallet-created")
+  record WalletCreated(String walletId) implements PublicWalletEvent {
+  }
+
+  @TypeName("balance-increased")
+  record Deposited(String walletId, long amount) implements PublicWalletEvent {
+  }
+
+  @TypeName("balance-decreased")
+  record Withdrawn(String walletId, long amount) implements PublicWalletEvent {
+  }
+
+}
