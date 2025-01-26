f2e49489e25512065226639b5ed7be918d144ec4
Renato Cavalcanti <renato@cavalcanti.io>
2025-01-26 21:39:21 +0100

    transfer auto-cancellation

---
+++ src/main/java/demo/transfer/application/consumers/TransferTimeoutConsumer.java
@@ -0,0 +1,66 @@
+package demo.transfer.application.consumers;
+
+import akka.javasdk.annotations.ComponentId;
+import akka.javasdk.annotations.Consume;
+import akka.javasdk.client.ComponentClient;
+import akka.javasdk.consumer.Consumer;
+import akka.javasdk.timer.TimerScheduler;
+import demo.transfer.application.TransferMediatorEntity;
+import demo.transfer.domain.TransferEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+@ComponentId("transaction-timeout")
+@Consume.FromEventSourcedEntity(TransferMediatorEntity.class)
+public class TransferTimeoutConsumer extends Consumer {
+
+  final private ComponentClient componentClient;
+  final private TimerScheduler timerScheduler;
+  final private Logger logger = LoggerFactory.getLogger(getClass());
+
+  public TransferTimeoutConsumer(ComponentClient componentClient, TimerScheduler timerScheduler) {
+    this.componentClient = componentClient;
+    this.timerScheduler = timerScheduler;
+  }
+
+
+  private String genTimerId(String transferId) {
+    return "timeout-transfer-timer:" + transferId;
+  }
+
+  public Effect onEvent(TransferEvent event) {
+    return switch (event) {
+
+      case TransferEvent.Created evt -> {
+
+        String timerId = genTimerId(evt.transferId());
+        logger.info("Scheduling cancellation for transfer [{}], timer id [{}]", evt.transferId(), timerId);
+        var cancellationCall =
+          componentClient
+            .forEventSourcedEntity(evt.transferId())
+            .method(TransferMediatorEntity::cancel)
+            .deferred();
+
+        var scheduledCancellation =
+          timerScheduler.startSingleTimer(timerId, Duration.ofSeconds(20), cancellationCall);
+
+        yield effects().asyncDone(scheduledCancellation);
+      }
+
+      case TransferEvent.Initiated evt -> cancelTimer(evt.transferId());
+      case TransferEvent.Completed evt -> cancelTimer(evt.transferId());
+      case TransferEvent.Cancelled evt -> cancelTimer(evt.transferId());
+
+      default -> effects().ignore();
+
+    };
+  }
+
+  private Effect cancelTimer(String txId) {
+    var timerId = genTimerId(txId);
+    logger.info("Cancelling transfer timer [{}]", timerId);
+    return effects().asyncDone(timerScheduler.cancel(timerId));
+  }
+}
