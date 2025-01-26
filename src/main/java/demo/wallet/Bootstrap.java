a837d3cb05fe9518441e0645f847c65c3bd954ca
Renato Cavalcanti <renato@cavalcanti.io>
2025-01-26 12:17:24 +0100

    remove demo consumers

--- src/main/java/demo/wallet/Bootstrap.java
+++ src/main/java/demo/wallet/Bootstrap.java
@@ -2,16 +2,8 @@ package demo.wallet;
 
 import akka.javasdk.ServiceSetup;
 import akka.javasdk.annotations.Setup;
-import demo.wallet.application.consumers.WalletEventsToTopic;
-
-import java.util.Set;
 
 @Setup
 public class Bootstrap implements ServiceSetup {
-  @Override
-  public Set<Class<?>> disabledComponents() {
-    return Set.of(
-      WalletEventsToTopic.class // disable the topic producer for demo purposes
-    );
-  }
+
 }
