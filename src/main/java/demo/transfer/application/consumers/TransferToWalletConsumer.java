2bb08f20b9adcef09096b865a0ac316da2f819f9
Renato Cavalcanti <renato@cavalcanti.io>
2025-05-16 14:58:36 +0200

    refactored consumer to use VT

--- src/main/java/demo/transfer/application/consumers/TransferToWalletConsumer.java
+++ src/main/java/demo/transfer/application/consumers/TransferToWalletConsumer.java
@@ -1,6 +1,5 @@
 package demo.transfer.application.consumers;
 
-import akka.Done;
 import akka.javasdk.annotations.ComponentId;
 import akka.javasdk.annotations.Consume;
 import akka.javasdk.client.ComponentClient;
@@ -8,9 +7,6 @@ import akka.javasdk.consumer.Consumer;
 import demo.transfer.application.TransferMediatorEntity;
 import demo.transfer.domain.TransferEvent;
 import demo.wallet.application.WalletEntity;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +24,7 @@ public class TransferToWalletConsumer extends Consumer {
   public Effect onCreated(TransferEvent event) {
 
     return switch (event) {
-        // only react on those three events
+        // only react to those three events
       case TransferEvent.Initiated evt -> initiate(evt);
       case TransferEvent.Completed evt -> complete(evt);
       case TransferEvent.Cancelled evt -> cancel(evt);
@@ -36,59 +32,46 @@ public class TransferToWalletConsumer extends Consumer {
     };
   }
 
-  private Effect allOrNothing(List<CompletionStage<Done>> allFutures) {
-    return effects()
-        .asyncDone(
-            CompletableFuture.allOf(allFutures.toArray(CompletableFuture[]::new))
-                .thenApply(__ -> Done.getInstance()));
-  }
-
   private Effect initiate(TransferEvent.Initiated evt) {
     logger.info("Executing transaction [{}]", evt.transferId());
 
-    var allFutures =
-        evt.participantsIds().stream()
-            .map(
-                walletId ->
-                    componentClient
-                        .forEventSourcedEntity(walletId)
-                        .method(WalletEntity::executeTransaction)
-                        .invokeAsync(evt.transferId()))
-            .toList();
+    evt.participantsIds()
+        .forEach(
+            walletId ->
+                componentClient
+                    .forEventSourcedEntity(walletId)
+                    .method(WalletEntity::executeTransaction)
+                    .invoke(evt.transferId()));
 
-    return allOrNothing(allFutures);
+    return effects().done();
   }
 
   private Effect complete(TransferEvent.Completed evt) {
     logger.info("Completing transaction [{}]", evt.transferId());
 
-    var allFutures =
-        evt.participantsIds().stream()
-            .map(
-                walletId ->
-                    componentClient
-                        .forEventSourcedEntity(walletId)
-                        .method(WalletEntity::completeTransaction)
-                        .invokeAsync(evt.transferId()))
-            .toList();
+    evt.participantsIds()
+        .forEach(
+            walletId ->
+                componentClient
+                    .forEventSourcedEntity(walletId)
+                    .method(WalletEntity::completeTransaction)
+                    .invoke(evt.transferId()));
 
-    return allOrNothing(allFutures);
+    return effects().done();
   }
 
   private Effect cancel(TransferEvent.Cancelled evt) {
 
     logger.info("Cancelling transaction [{}]", evt.transferId());
 
-    var allFutures =
-        evt.participantsIds().stream()
-            .map(
-                walletId ->
-                    componentClient
-                        .forEventSourcedEntity(walletId)
-                        .method(WalletEntity::cancelTransaction)
-                        .invokeAsync(evt.transferId()))
-            .toList();
+    evt.participantsIds()
+        .forEach(
+            walletId ->
+                componentClient
+                    .forEventSourcedEntity(walletId)
+                    .method(WalletEntity::cancelTransaction)
+                    .invoke(evt.transferId()));
 
-    return allOrNothing(allFutures);
+    return effects().done();
   }
 }
